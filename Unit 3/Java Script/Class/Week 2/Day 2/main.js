8528054d93084dacc4f25ff180844bce87ee2b1e
shoto2000 <tayyabsyed2000@gmail.com>
2022-05-29 14:47:25 +0530

    week2 day3 added

---
+++ Unit 3/Java Script/Class/Week 2/Day 2/main.js
@@ -0,0 +1,17 @@
+// Promise
+
+// 1. Pending State
+// 2. Completed
+// 3. Rejected
+
+// Time
+
+// Promise() //function
+
+// new Promise() //constructor function
+
+let p = new Promise(function(resolve,reject){
+    
+});
+// resolve and reject
+console.log("p:",p);
